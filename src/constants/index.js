33855be5a994bb0df8c20aa38ff46b7c8b0c1f48
unknown <ali.z.jalloul@gmail.com>
2024-07-15 13:50:52 +0300

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,70 @@
+export const hightlightsSlides = [
+  {
+    id: 1,
+    textLists: ["Enter B17 Pro Max.", "Stronk.", "Fast."],
+    video: "/assets/videos/highlight-first.mp4",
+    videoDuration: 4,
+  },
+  {
+    id: 2,
+    textLists: ["Nice Female Individuell :>"],
+    video: "/assets/videos/highlight-sec.mp4",
+    videoDuration: 2,
+  },
+  {
+    id: 3,
+    textLists: [
+      "wePhone 69 Pro Max has the",
+      "most number of cameras any",
+      "phone ever had. Stonks!",
+    ],
+    video: "/assets/videos/highlight-third.mp4",
+    videoDuration: 5,
+  },
+  {
+    id: 4,
+    textLists: ["Sooo Many Buttons.", "UwU"],
+    video: "/assets/videos/highlight-fourth.mp4",
+    videoDuration: 3.63,
+  },
+];
+
+export const models = [
+  {
+    id: 1,
+    title: "wePhone 69 Pro in Natural Titanium",
+    color: ["#8F8A81", "#ffe7b9", "#6f6c64"],
+    img: "/assets/images/yellow.jpg",
+  },
+  {
+    id: 2,
+    title: "wePhone 69 Pro in Blue Titanium",
+    color: ["#53596E", "#6395ff", "#21242e"],
+    img: "/assets/images/blue.jpg",
+  },
+  {
+    id: 3,
+    title: "wePhone 69 Pro in White Titanium",
+    color: ["#C9C8C2", "#ffffff", "#C9C8C2"],
+    img: "/assets/images/white.jpg",
+  },
+  {
+    id: 4,
+    title: "wePhone 69 Pro in Black Titanium",
+    color: ["#454749", "#3b3b3b", "#181819"],
+    img: "/assets/images/black.jpg",
+  },
+];
+
+export const sizes = [
+  { label: "You", value: "small" },
+  { label: "The guy she tells you not to worry about", value: "large" },
+];
+
+export const footerLinks = [
+  "Privacy Policy",
+  "Terms of Use",
+  "Sales Policy",
+  "Legal",
+  "Site Map",
+];
